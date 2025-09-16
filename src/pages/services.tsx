b6d8a89171706ce6979237ceb38854d47d4b2c02
ait-admin <aitzkh8@gmail.com>
2025-09-17 00:48:36 +0530

    njj

--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -125,7 +125,7 @@ const Services = () => {
   {/* Background video */}
   <div className="absolute inset-0 flex items-center justify-center">
     <video
-      className="w-full h-full object-cover"
+      className="rounded-lg w-[500px] h-[235px] md:w-full md:h-full object-cover object-center"
       autoPlay
       muted
       loop
